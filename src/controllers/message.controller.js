54b5821f423adec67cd963bb27650d208427f31b
nambs <angelonambs@gmail.com>
2024-04-04 08:03:52 +0300

    iSomeso v1.0

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,91 +1,102 @@
-const Message = require('../models/message')
-const { ObjectId } = require('mongoose').Types
+const Message = require("../models/message");
 // Dernier message par chaque utilisateur
 
 const getLastMessages = async (req, res) => {
   try {
-    const userId = req.user._id
-    const usersSet = new Set()
+    const userId = req.user._id;
+    const usersSet = new Set();
 
     // Trouver tous les utilisateurs avec qui l' utilisateur actuel a eu une conversation
-    const fromUsers = await Message.distinct('from', { to: userId })
-    const toUsers = await Message.distinct('to', { from: userId })
+    const fromUsers = await Message.distinct("from", { to: userId });
+    const toUsers = await Message.distinct("to", { from: userId });
 
-    fromUsers.forEach(user => usersSet.add(user.toString()))
-    toUsers.forEach(user => usersSet.add(user.toString()))
+    fromUsers.forEach((user) => usersSet.add(user.toString()));
+    toUsers.forEach((user) => usersSet.add(user.toString()));
 
-    const users = Array.from(usersSet)
-    
+    const users = Array.from(usersSet);
+
     // Recuperer les derniers messages pour chaque utilisateur
-    const messages = await Promise.all( users.map( async (otherUserId) => {
-      return (await Message.find({
-        $or: [
-          { from: userId, to: otherUserId },
-          { from: otherUserId, to: userId }
-        ]
-      }).sort({ createdAt: -1 }).limit(1).populate(['to', 'from']).exec())[0]
-    }))
-    res.send({ messages: messages })
+    const messages = await Promise.all(
+      users.map(async (otherUserId) => {
+        return (
+          await Message.find({
+            $or: [
+              { from: userId, to: otherUserId },
+              { from: otherUserId, to: userId },
+            ],
+          })
+            .sort({ createdAt: -1 })
+            .limit(1)
+            .populate(["to", "from"])
+            .exec()
+        )[0];
+      })
+    );
+    res.send({ messages: messages });
   } catch (error) {
-    res.status(500).send({ error: error })
+    res.status(500).send({ error: error });
   }
-}
+};
 
 // Discussion avec un utilisateur (tous les messages)
 
 const getMessages = async (req, res) => {
   try {
-    const _id = new ObjectId(req.params.id)
     const messages = await Message.find({
       $or: [
         { from: req.user._id, to: req.params.id },
-        { from: req.params.id, to: req.user._id }
-      ]
-    })
-    // .sort({ createdAt: -1 });
-    
+        { from: req.params.id, to: req.user._id },
+      ],
+    }).sort({ createdAt: 1 });
+
     // modifier tous les messages envoyer par l'autre utilisateur en vue
-    messages.forEach((message) => {
-      if (message.from == req.params.id && !message.vue) {
-        message.vue = true
-        message.save()
+    messages.forEach(async (message) => {
+      if (!message.vue && message.from == req.params.id) {
+        message.vue = true;
+        await message.save();
       }
-    })
-    res.send({ messages: messages })
+    });
+    res.send({ messages: messages });
   } catch (error) {
-    res.status(400).send({ error: error })
+    res.status(400).send({ error: error });
   }
-}
+};
 
 const sendMessage = (req, res) => {
   const message = new Message({
     ...req.body,
-    from: req.user._id
-  })
+    from: req.user._id,
+  });
 
-  message.save().then(() => {
-    res.status(201).send({ messages: message })
-  }).catch((error) => {
-    res.status(400).send({ error: error })
-  })
-}
+  message
+    .save()
+    .then(() => {
+      res.status(201).send({ message: message });
+    })
+    .catch((error) => {
+      res.status(400).send({ error: error });
+    });
+};
 
 const deleteMessage = async (req, res) => {
   try {
-   const message = await Message.findOneAndDelete({ _id: req.params.id, from: req.user._id})
-   
-   if(!message){
-    return res.status(404).send({ error: "Message invalide" })
-   }
-   res.send({ messages: message })
+    const message = await Message.findOneAndDelete({
+      _id: req.params.id,
+      from: req.user._id,
+    });
+
+    if (!message) {
+      return res.status(404).send({ error: "Message invalide" });
+    }
+    res.send({ messages: message });
   } catch (error) {
-    res.status(500).send({ error: error })
+    res.status(500).send({ error: error });
   }
-}
+};
 
 module.exports = {
   getLastMessages,
   getMessages,
   sendMessage,
-  deleteMessage
-}
+  deleteMessage,
+};
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '')
+    const token = req.header('Authorization').split(' ')[1]
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
     const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
 
